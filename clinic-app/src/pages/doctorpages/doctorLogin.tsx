b32f4c609e227181dd2c2913b41e6b40dd99786c
Kwan Bui Man Phoebe <feebsk@outlook.com>
2024-07-05 11:39:00 +0800

    resolve merge conflict, sth weird happened, some files missing from the latest main 2

--- clinic-app/src/pages/doctorpages/doctorLogin.tsx
+++ clinic-app/src/pages/doctorpages/doctorLogin.tsx
@@ -3,17 +3,17 @@ import logo from './logo.svg';
 import style from './App.module.scss';
 import TemporaryDrawer from '../../components/doctors/doctorNavBar';
 import { blue } from '@mui/material/colors';
-// import { ListPatients } from '../../components/doctors/patientList';
+import { ListPatients } from '../../components/doctors/patientList';
 import { Margin } from '@mui/icons-material';
 
 import DoctorLogInForm from '../../components/doctors/doctorLogInForm';
 
 export default function DoctorLogin() {
- 
+
   return (
     <div className="App">
       <header className="App-header">
-        
+
       </header>
       <DoctorLogInForm />
     </div>
