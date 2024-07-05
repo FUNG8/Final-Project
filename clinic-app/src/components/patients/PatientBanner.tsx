488256191713e7baa96d29a48e8fc5f711c5b601
Harvie <chengchifeng8@gmail.com>
2024-07-05 13:22:59 +0800

    rename file

---
+++ clinic-app/src/components/patients/PatientBanner.tsx
@@ -0,0 +1,34 @@
+// hahahahaha
+import React, { useEffect, useState } from 'react'
+import  "./PatientBanner.scss"
+import logo from '../../image/logo.jpeg'
+
+export default function PatientBanner(){
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+      // Handler to call on window resize
+      const handleResize = () => {
+        setWindowWidth(window.innerWidth);
+      };
+  
+      // Add event listener for window resize
+      window.addEventListener('resize', handleResize);
+  
+      // Clean up event listener on component unmount
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }, []);
+
+    const imageSize = windowWidth < 600 ? 100 : 150; // Example: smaller image for small screens
+    return(
+        <div className='bannerBack-layer'>
+            <img className='image' src={logo} alt="logo"
+            style={{
+            width: `${imageSize}px`,
+            height: `${imageSize}px`,
+        }}/>
+        </div>
+    )
+}
