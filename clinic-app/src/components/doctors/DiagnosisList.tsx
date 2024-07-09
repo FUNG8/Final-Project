7c0cca16302add7996c794112d15f6c30c74d5a8
Carson Lee <leelokhin0406@gmail.com>
2024-07-09 14:20:48 +0800

    fixed darkmode medicine insert

--- clinic-app/src/components/doctors/DiagnosisList.tsx
+++ clinic-app/src/components/doctors/DiagnosisList.tsx
@@ -21,14 +21,14 @@ import { SetStateAction, useEffect, useState } from "react";
 import { SearchBar } from "./SearchBar";
 import { useShowDiagnosis } from "../../api/diagnosisAPI";
 import { useParams } from "react-router-dom";
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
-import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 interface Diagnosis {
   id: string;
@@ -62,148 +62,130 @@ interface DiagnosisResponse {
 interface InstructionResponse {
   status: string;
   instructionsResult?: Instructions[];
-
 }
 
 export function ListDiagnosis() {
   let { patientId } = useParams();
-  const diagnosis: DiagnosisResponse | null = useShowDiagnosis(parseInt(patientId!));
-  const instructions: InstructionResponse | null = useShowDiagnosis(parseInt(patientId!));
-
-  // function createData(
-  //   method: string,
-  //   period_hr: number,
-  //   period_day: number,
-  //   medicine_id: number,
-  //   taken_count: number,
-  //   drug_remarks: string,
-  //   medicine_name: string,
-  //   total_quantity: number,
-  //   unit_measurement: number,
-  //   frequency_per_day: number,
-  //   taken_count_today: number,
-  //   dosage_per_serving: number
-  // ) {
-  //   return { 
-  //     medicine_id, 
-  //     medicine_name, 
-  //     method, period_hr, 
-  //     period_day, 
-  //     taken_count, 
-  //     drug_remarks, 
-  //     total_quantity, 
-  //     unit_measurement, 
-  //     frequency_per_day, 
-  //     taken_count_today, 
-  //     dosage_per_serving };
-  // }
-
-  // const rows = [
-  //   createData('Medicine_id ',{instrictopm}),
-  //   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  //   createData('Eclair', 262, 16.0, 24, 6.0),
-  //   createData('Cupcake', 305, 3.7, 67, 4.3),
-  //   createData('Gingerbread', 356, 16.0, 49, 3.9),
-  //   createData('Frozen yoghurt',),
-  //   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  //   createData('Eclair', 262, 16.0, 24, 6.0),
-  //   createData('Cupcake', 305, 3.7, 67, 4.3),
-  //   createData('Gingerbread', 356, 16.0, 49, 3.9),
-  //   createData('Gingerbread', 356, 16.0, 49, 3.9),
-  //   createData('Gingerbread', 356, 16.0, 49, 3.9)
-  // ];
-
+  const diagnosis: DiagnosisResponse | null = useShowDiagnosis(
+    parseInt(patientId!)
+  );
+  const instructions: InstructionResponse | null = useShowDiagnosis(
+    parseInt(patientId!)
+  );
 
   return (
-
     <div>
       <Box justifyContent="center" mt={4}>
-        {diagnosis?.status === "success" && diagnosis.diagnosisResult?.map((diagnosis) => (
-          <div key={diagnosis.id}>
-            <Accordion
-              sx={{
-                margin: 2,
-                "&.Mui-expanded": {
-                  // Styles for the expanded accordion
-                  boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
-                  backgroundColor: "#f5f5f5",
+        {diagnosis?.status === "success" &&
+          diagnosis.diagnosisResult?.map((diagnosis) => (
+            <div key={diagnosis.id}>
+              <Accordion
+                sx={{
                   margin: 2,
-                },
-              }}
-            >
-              <AccordionSummary
-                expandIcon={<ArrowDropDownIcon />}
-                aria-controls="panel2-content"
-                id="panel2-header"
+                  "&.Mui-expanded": {
+                    // Styles for the expanded accordion
+                    boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+                    backgroundColor: "#9a9ca1",
+
+                    margin: 2,
+                  },
+                }}
               >
-                <Grid xs={4} sx={{ margin: 2 }}>
-                  <Typography>Diagnosis ID: {diagnosis.id}</Typography>
-                </Grid>
-                <Grid xs={8} sx={{ margin: 2 }}>
-                  <Typography>Symptoms: {diagnosis.name}</Typography>
-                </Grid>
-              </AccordionSummary>
-              <Divider />
-              <AccordionDetails>
-                <Typography sx={{ margin: 2 }}>
-                  Remarks: {diagnosis.remarks}
-                </Typography>
+                <AccordionSummary
+                  expandIcon={<ArrowDropDownIcon />}
+                  aria-controls="panel2-content"
+                  id="panel2-header"
+                >
+                  <Grid xs={4} sx={{ margin: 2 }}>
+                    <Typography>Diagnosis ID: {diagnosis.id}</Typography>
+                  </Grid>
+                  <Grid xs={8} sx={{ margin: 2 }}>
+                    <Typography>Symptoms: {diagnosis.name}</Typography>
+                  </Grid>
+                </AccordionSummary>
                 <Divider />
-                <Typography sx={{ margin: 2 }}>
-                  Diagnosis Time: {diagnosis.created_at}
-                </Typography>
-                <Divider />
-                <Typography sx={{ margin: 2 }}>
-                  {/* mapping instructions */}
-                  {instructions?.status === "success" && instructions.instructionsResult?.map((instructions) => (
-
-                    <Paper key={instructions.instruction_id} sx={{ marginBottom: 2, padding: 2 }}>
-                      <Grid container sx={{ marginTop: 2, marginBottom: 2 }}>
-                        <Grid xs={2}>Medicine ID:</Grid>
-                        <Grid xs={2}>{instructions.medicine_id}</Grid>
-                        <Grid xs={2}>Medicine Name:</Grid>
-                        <Grid xs={6}>{instructions.medicine_name}</Grid>
-                      </Grid>
-                      <Grid container sx={{ marginTop: 2, marginBottom: 2 }}>
-                        <Grid xs={2}>Take In Method:</Grid>
-                        <Grid xs={2}>{instructions.method}</Grid>
-                        <Grid xs={2}>Take In Period Day:</Grid>
-                        <Grid xs={2}>{instructions.period_day}</Grid>
-                        <Grid xs={2}>Take In Period Hours:</Grid>
-                        <Grid xs={2}>{instructions.period_hr}</Grid>
-                      </Grid>
-                      <Grid container sx={{ marginTop: 2, marginBottom: 2 }}>
-                        <Grid xs={2}>Unit Measurement:</Grid>
-                        <Grid xs={2}>{instructions.unit_measurement}</Grid>
-                        <Grid xs={2}>Frequency Per Day:</Grid>
-                        <Grid xs={2}>{instructions.frequency_per_day}</Grid>
-                        <Grid xs={2}>Dosage Per Serving:</Grid>
-                        <Grid xs={2}>{instructions.dosage_per_serving}</Grid>
-                      </Grid>
-                      <Grid container sx={{ marginTop: 2, marginBottom: 2 }}>
-                        <Grid xs={2}>Drug Remarks:</Grid>
-                        <Grid xs={10}>{instructions.drug_remarks}</Grid>
-                      </Grid>
-                      <Divider />
-                      <Grid container sx={{ marginTop: 2, marginBottom: 2 }}>
-                        <Grid xs={2}>Total Quantity:</Grid>
-                        <Grid xs={2}><Chip label={instructions.total_quantity} /></Grid>
-                        <Grid xs={2}>Taken Count:</Grid>
-                        <Grid xs={2}><Chip label={instructions.taken_count} /></Grid>
-                        <Grid xs={2}>Taken Count Today:</Grid>
-                        <Grid xs={2}><Chip label={instructions.taken_count_today} /></Grid>
-                      </Grid>
-                    </Paper>
-                  ))}</Typography>
-
-
-              </AccordionDetails>
-            </Accordion>
-
-          </div>
-        ))}
-
-
+                <AccordionDetails>
+                  <Typography sx={{ margin: 2 }}>
+                    Remarks: {diagnosis.remarks}
+                  </Typography>
+                  <Divider />
+                  <Typography sx={{ margin: 2 }}>
+                    Diagnosis Time: {diagnosis.created_at}
+                  </Typography>
+                  <Divider />
+                  <Typography sx={{ margin: 2 }}>
+                    {/* mapping instructions */}
+                    {instructions?.status === "success" &&
+                      instructions.instructionsResult?.map((instructions) => (
+                        <Paper
+                          key={instructions.instruction_id}
+                          sx={{ marginBottom: 2, padding: 2 }}
+                        >
+                          <Grid
+                            container
+                            sx={{ marginTop: 2, marginBottom: 2 }}
+                          >
+                            <Grid xs={2}>Medicine ID:</Grid>
+                            <Grid xs={2}>{instructions.medicine_id}</Grid>
+                            <Grid xs={2}>Medicine Name:</Grid>
+                            <Grid xs={6}>{instructions.medicine_name}</Grid>
+                          </Grid>
+                          <Grid
+                            container
+                            sx={{ marginTop: 2, marginBottom: 2 }}
+                          >
+                            <Grid xs={2}>Take In Method:</Grid>
+                            <Grid xs={2}>{instructions.method}</Grid>
+                            <Grid xs={2}>Take In Period Day:</Grid>
+                            <Grid xs={2}>{instructions.period_day}</Grid>
+                            <Grid xs={2}>Take In Period Hours:</Grid>
+                            <Grid xs={2}>{instructions.period_hr}</Grid>
+                          </Grid>
+                          <Grid
+                            container
+                            sx={{ marginTop: 2, marginBottom: 2 }}
+                          >
+                            <Grid xs={2}>Unit Measurement:</Grid>
+                            <Grid xs={2}>{instructions.unit_measurement}</Grid>
+                            <Grid xs={2}>Frequency Per Day:</Grid>
+                            <Grid xs={2}>{instructions.frequency_per_day}</Grid>
+                            <Grid xs={2}>Dosage Per Serving:</Grid>
+                            <Grid xs={2}>
+                              {instructions.dosage_per_serving}
+                            </Grid>
+                          </Grid>
+                          <Grid
+                            container
+                            sx={{ marginTop: 2, marginBottom: 2 }}
+                          >
+                            <Grid xs={2}>Drug Remarks:</Grid>
+                            <Grid xs={10}>{instructions.drug_remarks}</Grid>
+                          </Grid>
+                          <Divider />
+                          <Grid
+                            container
+                            sx={{ marginTop: 2, marginBottom: 2 }}
+                          >
+                            <Grid xs={2}>Total Quantity:</Grid>
+                            <Grid xs={2}>
+                              <Chip label={instructions.total_quantity} />
+                            </Grid>
+                            <Grid xs={2}>Taken Count:</Grid>
+                            <Grid xs={2}>
+                              <Chip label={instructions.taken_count} />
+                            </Grid>
+                            <Grid xs={2}>Taken Count Today:</Grid>
+                            <Grid xs={2}>
+                              <Chip label={instructions.taken_count_today} />
+                            </Grid>
+                          </Grid>
+                        </Paper>
+                      ))}
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+            </div>
+          ))}
       </Box>
     </div>
   );
@@ -211,4 +193,3 @@ export function ListDiagnosis() {
 function setSearchTerm(input: string) {
   throw new Error("Function not implemented.");
 }
-
--- clinic-app/src/pages/doctorpages/Home.tsx
+++ clinic-app/src/pages/doctorpages/Home.tsx
@@ -1,21 +1,21 @@
 // hahahahaha
-import './Home.scss';
-import TemporaryDrawer from '../../components/doctors/DoctorNavBar';
-import * as React from 'react';
-import { useState, useEffect } from 'react';
-import Box from '@mui/material/Box';
-import ConsultTable from '../../components/patients/ConsultingTable'
-import styled from 'styled-components';
-import { FormControlLabel, Paper, Slide, Switch } from '@mui/material';
-import Grid from '@mui/material/Grid';
-import { Username } from '../../components/patients/Username';
-import ManyContainer from './ManyContainer';
-import HomeTable from '../../components/patients/HomeTable';
-import { BarChart } from '@mui/x-charts/BarChart';
-import { LineChart } from '@mui/x-charts';
-import MedicineConsumption from '../../components/doctors/MedicineConsumption';
-import PatientNumber from '../../components/doctors/PatientNumber';
-import { Margin } from '@mui/icons-material';
+import "./Home.scss";
+import TemporaryDrawer from "../../components/doctors/DoctorNavBar";
+import * as React from "react";
+import { useState, useEffect } from "react";
+import Box from "@mui/material/Box";
+import ConsultTable from "../../components/patients/ConsultingTable";
+import styled from "styled-components";
+import { FormControlLabel, Paper, Slide, Switch } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import { Username } from "../../components/patients/Username";
+import ManyContainer from "./ManyContainer";
+import HomeTable from "../../components/patients/HomeTable";
+import { BarChart } from "@mui/x-charts/BarChart";
+import { LineChart } from "@mui/x-charts";
+import MedicineConsumption from "../../components/doctors/MedicineConsumption";
+import PatientNumber from "../../components/doctors/PatientNumber";
+import { Margin } from "@mui/icons-material";
 
 const icon = (
   <Paper sx={{ m: 1, width: 100, height: 100 }} elevation={4}>
@@ -33,7 +33,6 @@ const icon = (
   </Paper>
 );
 
-
 export default function Home() {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
@@ -44,11 +43,11 @@ export default function Home() {
     };
 
     // Add event listener for window resize
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     // Clean up event listener on component unmount
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
@@ -64,43 +63,35 @@ export default function Home() {
   //   margin: 8
   // }});
 
-
-
   return (
-
     <div className="App">
       <Grid sx={{ paddingLeft: 20 }}>
         <Username />
       </Grid>
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={2}>
 
-          <Grid item xs={8}>
-
-            <Grid sx={{ display: "flex" }}>
-              <Paper>
-                <p>Number of Patients</p>
-                <PatientNumber />
-              </Paper>
-              <Paper>
-                <p>Medicine Consumption</p>
-                <MedicineConsumption />
-              </Paper>
-            </Grid>
-
-          </Grid>
-          <Grid item xs={4}>
-            <Paper> <div className="waitingList">Consulting</div>
-              <ConsultTable />
-              <div className="waitingList">Waiting List</div>
-              <HomeTable />
+      <Grid container >
+        <Grid px={10} item xs={8}>
+          <Grid spacing={4} sx={{ display: "flex" }}>
+            <Paper>
+              <p>Number of Patients</p>
+              <PatientNumber />
+            </Paper>
+            <Paper>
+              <p>Medicine Consumption</p>
+              <MedicineConsumption />
             </Paper>
           </Grid>
-
         </Grid>
-      </Box>
-
+        <Grid px={10} item xs={4}>
+          <Paper>
+            {" "}
+            <div className="waitingList">Consulting</div>
+            <ConsultTable />
+            <div className="waitingList">Waiting List</div>
+            <HomeTable />
+          </Paper>
+        </Grid>
+       </Grid>
     </div>
-
   );
 }
